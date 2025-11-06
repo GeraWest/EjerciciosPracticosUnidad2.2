06e18b3f5aa50d548e34baf77ef1f1ec2674ee20
GeraWest <1224100509.rgam@gmail.com>
2025-11-05 23:18:18 -0600

    Update ejer9_RevertirLista.java

--- ejer9_RevertirLista.java
+++ ejer9_RevertirLista.java
@@ -1,13 +1,21 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package pilas;
+
+import java.util.Stack;
 
 /**
  *
- * @author 12241
+ * @author Avalos Mellado Rey Gerardo
+ * 1224100509
+ * GTID0141
  */
 public class ejer9_RevertirLista {
+    public static void main(String[] args) {
+int[] lista = {1, 2, 3, 4};
+Stack<Integer> pila = new Stack<>();
+for (int n : lista) pila.push(n);
+System.out.print("Lista invertida: ");
+while (!pila.isEmpty()) System.out.print(pila.pop() + " ");
+System.out.println();
+}
     
 }
