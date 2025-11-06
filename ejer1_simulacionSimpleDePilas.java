fc2f0de19a71830cea0eaebfc980cefee5964855
GeraWest <1224100509.rgam@gmail.com>
2025-11-05 23:14:14 -0600

    Update ejer1_simulacionSimpleDePilas.java

--- ejer1_simulacionSimpleDePilas.java
+++ ejer1_simulacionSimpleDePilas.java
@@ -1,13 +1,23 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package pilas;
+
+import java.util.Stack;
 
 /**
  *
- * @author 12241
+ * @author Avalos Mellado Rey Gerardo
+ * 1224100509
+ * GTID0141
  */
 public class ejer1_simulacionSimpleDePilas {
-    
+
+ public static void main(String[] args) {
+ Stack<Integer> pila = new Stack<>();
+ pila.push(5);
+ pila.push(10);
+ pila.push(15);
+ pila.push(20);
+ pila.pop();
+ pila.pop();
+ System.out.println("Contenido actual: " + pila);
+ }
 }
