4905b6ebc7159cfebb01b6f76961b030ce9ba3f4
GeraWest <1224100509.rgam@gmail.com>
2025-11-05 23:15:46 -0600

    Update ejer4_invertirUnaPalabra.java

--- ejer4_invertirUnaPalabra.java
+++ ejer4_invertirUnaPalabra.java
@@ -1,13 +1,23 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package pilas;
 
+import java.util.Scanner;
+import java.util.Stack;
 /**
  *
- * @author 12241
+ * @author Avalos Mellado Rey Gerardo
+ * 1224100509
+ * GTID0141
  */
 public class ejer4_invertirUnaPalabra {
-    
+    public static void main(String[] args) {
+Scanner sc = new Scanner(System.in);
+System.out.print("Ingrese una palabra: ");
+String palabra = sc.nextLine();
+Stack<Character> pila = new Stack<>();
+for (char c : palabra.toCharArray()) pila.push(c);
+System.out.print("Invertida: ");
+while (!pila.isEmpty()) System.out.print(pila.pop());
+System.out.println();
+sc.close();
+     }
 }
