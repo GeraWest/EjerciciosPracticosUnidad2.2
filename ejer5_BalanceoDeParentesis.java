a85f076106fefe89c59212469dcc8514daa6ae34
GeraWest <1224100509.rgam@gmail.com>
2025-11-05 23:16:22 -0600

    Update ejer5_BalanceoDeParentesis.java

--- ejer5_BalanceoDeParentesis.java
+++ ejer5_BalanceoDeParentesis.java
@@ -1,13 +1,30 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package pilas;
+
+import java.util.Stack;
 
 /**
  *
- * @author 12241
+ * @author Avalos Mellado Rey Gerardo
+ * 1224100509
+ * GTID0141
  */
 public class ejer5_BalanceoDeParentesis {
-    
+    public static boolean estaBalanceada(String expr) {
+Stack<Character> pila = new Stack<>();
+for (char c : expr.toCharArray()) {
+if (c == '(') pila.push(c);
+else if (c == ')') {
+if (pila.isEmpty()) return false;
+pila.pop();
 }
+}
+return pila.isEmpty();
+}
+
+
+public static void main(String[] args) {
+System.out.println(estaBalanceada("((2+3)*5)")); // true
+System.out.println(estaBalanceada("((2+3)*5")); // false
+}
+}
+
