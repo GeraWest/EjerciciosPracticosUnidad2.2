d3654e806f067891e559256a5c58d9919a50537d
GeraWest <1224100509.rgam@gmail.com>
2025-11-05 23:18:53 -0600

    Update ejer10_VerificarPalindromo.java

--- ejer10_VerificarPalindromo.java
+++ ejer10_VerificarPalindromo.java
@@ -1,13 +1,27 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package pilas;
+
+import java.util.Scanner;
+import java.util.Stack;
 
 /**
  *
- * @author 12241
+ * @author Avalos Mellado Rey Gerardo
+ * 1224100509
+ * GTID0141
  */
 public class ejer10_VerificarPalindromo {
-    
+    public static void main(String[] args) {
+Scanner sc = new Scanner(System.in);
+System.out.print("Ingrese palabra: ");
+String palabra = sc.nextLine();
+Stack<Character> pila = new Stack<>();
+for (char c : palabra.toCharArray()) pila.push(c);
+StringBuilder invertida = new StringBuilder();
+while (!pila.isEmpty()) invertida.append(pila.pop());
+if (palabra.equalsIgnoreCase(invertida.toString()))
+System.out.println("Es palíndromo");
+else
+System.out.println("No es palíndromo");
+sc.close();
+}
 }
