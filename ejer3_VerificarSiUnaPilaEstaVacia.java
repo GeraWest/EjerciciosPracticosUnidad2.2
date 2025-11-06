60c1515a7282ea315dccfb32fb93570a923f3ec2
GeraWest <1224100509.rgam@gmail.com>
2025-11-05 23:15:18 -0600

    Update ejer3_VerificarSiUnaPilaEstaVacia.java

--- ejer3_VerificarSiUnaPilaEstaVacia.java
+++ ejer3_VerificarSiUnaPilaEstaVacia.java
@@ -1,13 +1,20 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package pilas;
+
+import java.util.Stack;
 
 /**
  *
- * @author 12241
+ * @author Avalos Mellado Rey Gerardo
+ * 1224100509
+ * GTID0141
  */
 public class ejer3_VerificarSiUnaPilaEstaVacia {
+    public static void main(String[] args) {
+Stack<Integer> pila = new Stack<>();
+System.out.println("¿Está vacía la pila? " + pila.isEmpty());
+pila.push(1);
+System.out.println("¿Está vacía la pila? " + pila.isEmpty());
+ }
     
 }
+
