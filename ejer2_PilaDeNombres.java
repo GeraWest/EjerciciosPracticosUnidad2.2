a1fec71e842528e45d9435e3e09c03dec2fc71bc
GeraWest <1224100509.rgam@gmail.com>
2025-11-05 23:14:49 -0600

    Update ejer2_PilaDeNombres.java

--- ejer2_PilaDeNombres.java
+++ ejer2_PilaDeNombres.java
@@ -1,13 +1,37 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package pilas;
+
+import java.util.Scanner;
+import java.util.Stack;
 
 /**
  *
- * @author 12241
+ * @author Avalos Mellado Rey Gerardo
+ * 1224100509
+ * GTID0141
  */
 public class ejer2_PilaDeNombres {
-    
+    public static void main(String[] args) {
+Scanner sc = new Scanner(System.in);
+Stack<String> pila = new Stack<>();
+String nombre;
+
+
+while (true) {
+System.out.print("Ingrese un nombre (FIN para salir): ");
+nombre = sc.nextLine();
+if (nombre.equalsIgnoreCase("FIN")) break;
+if (!nombre.trim().isEmpty()) pila.push(nombre);
+     }
+
+
+System.out.println("Nombres en orden inverso:");
+while (!pila.isEmpty()) {
+System.out.println(pila.pop());
+
+     }
+
+
+sc.close();
+     }
 }
+  
