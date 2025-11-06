4c56e593801cfca0e3225dc56351f184e3944a81
GeraWest <1224100509.rgam@gmail.com>
2025-11-05 23:17:25 -0600

    Update ejer7_SimularFuncionDeshacer.java

--- ejer7_SimularFuncionDeshacer.java
+++ ejer7_SimularFuncionDeshacer.java
@@ -1,13 +1,34 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package pilas;
+
+import java.util.Scanner;
+import java.util.Stack;
 
 /**
  *
- * @author 12241
+ * @author Avalos Mellado Rey Gerardo
+ * 1224100509
+ * GTID0141
  */
 public class ejer7_SimularFuncionDeshacer {
-    
+    public static void main(String[] args) {
+Scanner sc = new Scanner(System.in);
+Stack<String> acciones = new Stack<>();
+
+
+while (true) {
+System.out.print("Acción (UNDO/FIN): ");
+String act = sc.nextLine();
+if (act.equalsIgnoreCase("FIN")) break;
+if (act.equalsIgnoreCase("UNDO")) {
+if (!acciones.isEmpty()) acciones.pop();
+else System.out.println("Nada que deshacer");
+} else {
+acciones.push(act);
+}
+System.out.println("Actual: " + acciones);
+}
+
+
+sc.close();
+}
 }
